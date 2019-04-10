27b8b78c4c7d21b211a18239437e342895023400
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-10 11:10:57 -0300

    Made the test result more verbose

--- test/test_fill_connection_data_from_string.c
+++ test/test_fill_connection_data_from_string.c
@@ -15,23 +15,37 @@ int compare_two_strings(char * a, char * b) {
 int execute_unit_test(int id, char * parameter, char * expected_host, int expected_port) {
 	rfc_connection_data data;
 
-	rfc_fill_connection_data_from_string(parameter, &data);
+	int result = rfc_fill_connection_data_from_string(parameter, &data);
+
+	if (result != 1) {
+		printf("Error at sub-test %d:\n", id);
+		printf("Input : \"%s\" (%d bytes)\n", parameter, strlen(parameter));
+		printf("Output: Error Code %d\n", result);
+		printf("Expect: \"%s\" (%d bytes)\n", expected_host, strlen(expected_host));
+		exit(1);
+	}
 
 	if (!compare_two_strings(data.host, expected_host)) {
-		printf("[%d] Expected host to be: '%s' Got: '%s'\n", id, expected_host, data.host);
+		printf("Error at sub-test %d:\n", id);
+		printf("Input : \"%s\" (%d bytes)\n", parameter, strlen(parameter));
+		printf("Output: \"%s\" (%d bytes)\n", data.host, strlen(data.host));
+		printf("Expect: \"%s\" (%d bytes)\n", expected_host, strlen(expected_host));
 		exit(1);
 	}
 
 	if (data.port != expected_port) {
-		printf("[%d] Expected port to be: '%d' Got: '%d'\n", id, expected_port, data.port);
+		printf("Error at sub-test %d:\n", id);
+		printf("Input : \"%s\" (%d bytes)\n", parameter, strlen(parameter));
+		printf("Output: %d\n", data.port);
+		printf("Expect: %d\n", expected_port);
 		exit(1);
 	}
 }
 
 int main() {
 	execute_unit_test(1, "localhost", "localhost", RFC_DEFAULT_PORT);
-	execute_unit_test(1, "localhost:8080", "localhost", 8080);
-	execute_unit_test(1, "google.com:884", "google.com", 884);
-	execute_unit_test(1, ":8", "", 8);
+	execute_unit_test(2, "localhost:8080", "localhost", 8080);
+	execute_unit_test(3, "google.com:884", "google.com", 884);
+	execute_unit_test(4, ":8", "", 8);
 	return 0;
 }
