80ee5ec488b7c03fa2177961c75752ae98a6914e
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-10 10:18:36 -0300

    Added test for "fill connection data string"

--- test/test_fill_connection_data_from_string.c
+++ test/test_fill_connection_data_from_string.c
@@ -18,18 +18,20 @@ int execute_unit_test(int id, char * parameter, char * expected_host, int expect
 	rfc_fill_connection_data_from_string(parameter, &data);
 
 	if (!compare_two_strings(data.host, expected_host)) {
-		printf("Test[%d] - Expected host to be: '%s' Got: '%s'\n", id, expected_host, data.host);
+		printf("[%d] Expected host to be: '%s' Got: '%s'\n", id, expected_host, data.host);
 		exit(1);
 	}
 
 	if (data.port != expected_port) {
-		printf("Test[%d] - Expected port to be: '%d' Got: '%d'\n", id, expected_port, data.port);
+		printf("[%d] Expected port to be: '%d' Got: '%d'\n", id, expected_port, data.port);
 		exit(1);
 	}
 }
 
 int main() {
-	printf("done it\n");
 	execute_unit_test(1, "localhost", "localhost", RFC_DEFAULT_PORT);
+	execute_unit_test(1, "localhost:8080", "localhost", 8080);
+	execute_unit_test(1, "google.com:884", "google.com", 884);
+	execute_unit_test(1, ":8", "", 8);
 	return 0;
 }
