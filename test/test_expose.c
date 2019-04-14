4351430f05b23f946a60aec0adafd599e5ad452d
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-14 07:36:04 -0300

    Created failing test for expose

--- test/test_expose.c
+++ test/test_expose.c
@@ -8,43 +8,66 @@
 #define BUFFER_VERS_ADDR	0
 #define BUFFER_SIZE_ADDR	1
 
+int value;
+
 int foo1(int a) {
-	printf("Foo is %d\n", a);
+	value += a;
 	return 7;
 }
 
 int foo2(int * n, int n_length) {
-	//rfc_print_bin_buffer((char *) n, sizeof(int));
-	printf("Foo2 - %d arguments, starting with %d\n", n_length, n[0]);
+	value += n_length;
+	return 5;
 }
 
 int main() {
-	rfc_expose("int foo1(int)", foo1);
-	rfc_expose("int foo2(int*, int)", foo2);
+	if (rfc_func_root.func != 0) {
+		printf("Test failed at sub-test 1:\nRoot function node is not null\n");
+		exit(1);
+	}
+	if (!rfc_expose("int foo1(int)", foo1)) {
+		printf("Test failed at sub-test 2:\nThe first expose returned an error code\n");
+		exit(1);
+	}
+	if (rfc_func_root.func == 0) {
+		printf("Test failed at sub-test 3:\nRoot function node is still null after expose\n");
+		exit(1);
+	}
+	if (rfc_func_root.next != 0) {
+		printf("Test failed at sub-test 4:\nRoot function node has next, but the list should only have one entry\n");
+		exit(1);
+	}
+	if (!rfc_expose("int foo2(int*, int)", foo2)) {
+		printf("Test failed at sub-test 5:\nThe second expose returned an error code\n");
+		exit(1);
+	}
+	if (rfc_func_root.next == 0) {
+		printf("Test failed at sub-test 6:\nRoot function node is still null after expose\n");
+		exit(1);
+	}
+	if (rfc_func_root.next->func == 0) {
+		printf("Test failed at sub-test 7:\nThe second function node is null after second expose\n");
+		exit(1);
+	}
+	value = 0;
+	int (*fun_1)(int);
+	int (*fun_2)(int*, int);
 
+	fun_1 = rfc_func_root.func;
+	fun_2 = rfc_func_root.next->func;
 
-	/*
-	char buffer[256];
-
-	buffer[BUFFER_VERS_ADDR] = 1;
-	buffer[BUFFER_SIZE_ADDR] = 3;
-	buffer[2] = RFC_INT;
-	int * aux = (int *) &buffer[2];
-	aux = aux + 1;
-	*aux = 1;
-	aux = aux + 1;
-	*aux = 77;
-	*(aux+1) = 10;
-
-	rfc_print_bin_buffer(&buffer[3], sizeof(int));
-	printf("\n");
-
-	printf("Seding length %d\n", (int) buffer[3+sizeof(int)-1]);
-	printf("And then %d\n", (int) buffer[3+sizeof(int)*2-1]);
-
-	rfc_call_exposed_function("foo2", &buffer[3]);
-
-	*/
+	if (fun_1(3) != 7) {
+		printf("Test failed at sub-test 8:\nThe root exposed function foo1 returned an unexpected number\n");
+		exit(1);
+	}
+	if (fun_2(0, 4) != 5) {
+		printf("Test failed at sub-test 9:\nThe second exposed function foo2 returned an unexpected number\n");
+		exit(1);
+	}
+	if (value != 3+4) {
+		printf("Test failed at sub-test 10:\nThe operation of both functions did not change the global value correctly\n");
+		exit(1);
+	}
 
 	return 0;
 }
