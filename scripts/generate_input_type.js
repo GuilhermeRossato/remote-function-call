da5938e4ec568b546bb2e1ebabd8080a83350708
Guilherme Rossato <guilhermrossato@gmail.com>
2019-04-18 08:03:08 -0300

    Added hex logic to parameters enum
    So that it's easier to test for them

--- scripts/generate_input_type.js
+++ scripts/generate_input_type.js
@@ -8,17 +8,59 @@ const { recursively_generate_descriptor_list, get_options_from_type_array } = re
 
 generate_file(["int", "char"], 4);
 
-function generate_enum(type_descriptor_list = [{ type: 'inta', parameter: 'int*' }]) {
+function generate_hex_from_list(type_list=["int", "inta"], maxCount=2, descriptor) {
+	if (!descriptor.startsWith("rfc_")) {
+		return "unhandled: "+descriptor;
+	} else if (descriptor == "rfc_unknown") {
+		return "0x00000000";
+	} else if (descriptor == "rfc_void") {
+		return "0x00000001";
+	}
+	var array = descriptor.substr(4).split("_");
+	while (array.length < maxCount) {
+		array.push("void");
+	}
+	const byteArray = array.reverse().map(function(type) {
+		let count = 1;
+		if (type == "void") {
+			return 0;
+		}
+		for (let index in type_list) {
+			if (type_list[index] == type) {
+				return count;
+			}
+			count++;
+		}
+		return 0;
+	});
+	return "0x0"+byteArray.join("").padStart(6, "0")+"0";
+}
+
+function generate_enum(type_list=["int", "inta"], type_descriptor_list = [{ type: 'inta', parameter: 'int*' }]) {
 	const list = type_descriptor_list;
 	const lines = [];
 	lines.push("enum rfc_input_type {");
 	lines.push("\trfc_unknown,");
 	lines.push("\trfc_void,");
-	lines.push("");
+	let maxLength = 0;
+	let maxCount = 0;
 	for (let index in list) {
 		lines.push("\trfc_"+list[index].type+((index == list.length-1)?"":","));
+		if (lines[lines.length-1].length > maxLength) {
+			maxLength = lines[lines.length-1].length;
+		}
+		if (list[index].type.split("_").length > maxCount) {
+			maxCount = list[index].type.split("_").length;
+		}
 	}
 	lines.push("}");
+	for (let index in lines) {
+		if (index < 1 || index >= lines.length-1) {
+			continue;
+		}
+		let lineBlocks = lines[index].split(",");
+		lines[index] = lineBlocks[0].padEnd(maxLength, " ")+" = "+generate_hex_from_list(type_list, maxCount, lineBlocks[0].trim())+","+(lineBlocks[1]?lineBlocks[1]:"");
+	}
 	return lines.join("\n")+"\n";
 }
 
@@ -45,8 +87,7 @@ function generate_function(type_descriptor_list = [{ type: 'inta', parameter: 'i
 			if (letter != list[index].parameter[0]) {
 				continue;
 			}
-			const linePrefix = "\t\t";
-			linePrefix += isFirst?"if": "} else if";
+			const linePrefix = "\t\t"+(isFirst?"if": "} else if");
 			if (isFirst) {
 				isFirst = false;
 			}
@@ -58,20 +99,20 @@ function generate_function(type_descriptor_list = [{ type: 'inta', parameter: 'i
 	});
 	lines.push("\t}");
 	lines.push("\treturn rfc_unknown;");
-	lines.push("}");
+	lines.push("};");
 	return lines.join("\n")+"\n";
 }
 
 function generate_file(type_array_list = ["int", "char"], max_depth = 2) {
-	const opts = get_options_from_type_array(["int", "char"]);
-	const list = recursively_generate_descriptor_list(opts, 0, max_depth);
+	const type_list = get_options_from_type_array(type_array_list);
+	const list = recursively_generate_descriptor_list(type_list, 0, max_depth);
 
 	const writeFileSync = (typeof fs === "object" ? fs : require("fs")).writeFileSync;
 	const join = (typeof path === "object" ? path : require("path")).join;
 	const targetFilePath = join(__dirname, "..", "src", "rfc_input_type.c");
 
 	const prefix = "/**\n * This file was generated automatically by the command \"npm run generate-input-type\".\n * Do not change it manually.\n */\n\n";
-	const enum_declaration = generate_enum(list);
+	const enum_declaration = generate_enum(type_list, list);
 	const function_declaration = generate_function(list);
 	const content = prefix + enum_declaration + "\n" + function_declaration;
 	writeFileSync(targetFilePath, content, "utf-8");
